c270e38aa32f5a4f09171d0f592a52c89d03b04e
schneidermanj <jennis@id.iit.edu>
2017-04-20 19:48:36 -0500

    Sky and grass
    added sky and grass elements

--- script.js
+++ script.js
@@ -1,2 +1,31 @@
 // Do your work in this file.
 
+//1. Create a `div` element with a class name of "**view**" and append it to the document `body`.
+var skyElement = document.createElement("div");
+skyElement.className = "view";
+document.body.appendChild(skyElement);
+
+//1. Create a `div` element with a class name of "**grass**" and append it to the previously created "view" div from step 3.
+var grassElement = document.createElement("div");
+grassElement.className = "grass";
+skyElement.appendChild(grassElement);
+
+// Create a `div` element with a class name of "**sun**" and append it to the previously created "view" div from step 3.
+var sunElement = document.createElement("div");
+sunElement.className = "sun";
+
+
+
+// Write a [`while`] loop to create 5 divs. The div classes will be "**ray-1**", "**ray-2**", "**ray-3**", "**ray-4**", and "**ray-5**". Append each of these divs to the previously created "sun" div from step 5.
+
+
+
+// Create a `div` element with a class name of "**mountain**" and append it to the previously created "view" div from step 3.
+
+
+
+// Create a `div` element with a class name of "**mountain-top**" and append it to the previously created "mountain" div from step 7.
+
+
+
+// Write a [`do...while`] loop to create 3 divs. The div classes will be "**mountain-cap-1**", "**mountain-cap-2**", and "**mountain-cap-3**". Append each of these divs to the previously created "mountain-top" div from step 8.
